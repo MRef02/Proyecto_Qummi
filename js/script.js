636ce93871fcf9ebe0ba931e3aa1fff8c94042ec
MRef02 <marifercan73@gmail.com>
2025-04-29 09:11:53 -0600

    AMillie2.0
    puedeserBR

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function jugar() {
+    alert("¡Comenzando el juego!");
+    
+  }
+  
+  function opciones() {
+    alert("Opciones aún no disponibles.");
+  }
+  
+  function salir() {
+    const confirmar = confirm("¿Seguro que quieres salir?");
+    if (confirmar) {
+      window.close(); 
+    }
+  }
+  
